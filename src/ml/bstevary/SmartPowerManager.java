28c6a314397896c93ddbe538a645fe5fa419826b
bstevary <bstevary@gmail.com>
2022-04-12 12:31:16 +0300

    powemaneger rectivication

--- src/ml/bstevary/SmartPowerManager.java
+++ src/ml/bstevary/SmartPowerManager.java
@@ -1,42 +1,35 @@
 package ml.bstevary;
 
-
 import java.util.Scanner;
 
-public class PowerManager {
-   static int A,B,C;
-
-    final  static int MITERLIMIT =1200,SEC_PER_HOUR=3600, SEC_PER_MIN=60, COST_1_30_MIN=10,COST_AFTER_30_MIN=5;
-
-
-    public static void main(String[] ARs) {
-        inputs();
-       calculator();
-    }
+public class SmartPowerManager {
+    static int A,B,C;
+    final  static int  MIN_PER_HOUR=60, COST_1_30_MIN=10,COST_AFTER_30_MIN=5;
+   static int MITERLIMIT =1200,totalBilling,billing4A,billing4B,billing4C;
+   static int time4A,time4B,time4C;
 
 
-    static void calculator(){
-        int totalBilling,billing4A,billing4B,billing4C, timeASec,timeBSec ,timeCSec;
-        float timeAHrs,timeBHrs ,timeCHrs,lastTime ;
-        timeASec= ratingToSec(A);
-        timeBSec= ratingToSec(B);
-        timeCSec= ratingToSec(C);
+    public static void main(String[] args)
+    {
+        float timeAHrs,timeBHrs,timeCHrs,lastTime;
 
-        timeAHrs=toHours(timeASec);
-        timeBHrs=toHours(timeBSec);
-        timeCHrs=toHours(timeCSec);
+           inputs();
+           calculator();
+           timeAHrs=toHours(time4A);
+             timeBHrs=toHours(time4B);
+            timeCHrs=toHours(time4A);
 
-        lastTime=timeCHrs>(timeAHrs>timeBHrs?timeAHrs:timeBHrs)?timeCHrs:(timeAHrs>timeBHrs?timeAHrs:timeBHrs);
+            billing4A=billing(time4A);
+            billing4B=billing(time4B);
+            billing4C=billing(time4C);
+            totalBilling=billing4A+billing4B+billing4C;
 
-        billing4A=billing(timeASec);
-        billing4B=billing(timeBSec);
-        billing4C=billing(timeCSec);
-        totalBilling=billing4A+billing4B+billing4C;
+
         System.out.println("Device A will be off after"+" "+timeAHrs+" hours");
         System.out.println("Device B will be off after"+" "+timeBHrs+" hours");
         System.out.println("Device C will be off after"+" "+timeCHrs+" hours");
         System.out.println("###############################################");
-
+        lastTime=timeCHrs>(timeAHrs>timeBHrs?timeAHrs:timeBHrs)?timeCHrs:(timeAHrs>timeBHrs?timeAHrs:timeBHrs);
         System.out.println("After"+" "+lastTime+" hours all devices will be off");
         System.out.println("###############################################");
 
@@ -47,23 +40,51 @@ public class PowerManager {
 
         System.out.println("So you will have to pay Ksh."+totalBilling+" for power");
         System.out.println("###############################################");
+
     }
-    static int billing(int timeInSec){
+    static int billing(int timeInMin){
         int billing=0;
-        if (timeInSec>=30)
+        if (timeInMin>=30)
             billing=COST_1_30_MIN*30;
-        if (timeInSec>30)
-            billing+=COST_AFTER_30_MIN*(timeInSec-30);
-        if (timeInSec<30)
-            billing=timeInSec*COST_1_30_MIN;
+        if (timeInMin>30)
+            billing+=COST_AFTER_30_MIN*(timeInMin-30);
+        if (timeInMin<30)
+            billing=timeInMin*COST_1_30_MIN;
         return billing;
 
     }
-    static float toHours(int timeSec){
-        return (float) timeSec/(SEC_PER_HOUR);
+    static float toHours(int timeInMin) {
+        return (float) timeInMin / MIN_PER_HOUR;
     }
-    static int ratingToSec(int rating){
-        return (MITERLIMIT *SEC_PER_MIN)/rating;
+
+    static void calculator(){
+        time4A=time4B=time4C=0;
+        while (MITERLIMIT>0){
+            if (MITERLIMIT>=A){
+               MITERLIMIT= MITERLIMIT - A;
+               time4A++;
+                System.out.println(MITERLIMIT);
+            }
+
+            if (MITERLIMIT>=B){
+                MITERLIMIT=MITERLIMIT-B;
+                time4B++;
+                System.out.println(MITERLIMIT);
+            }
+
+            if (MITERLIMIT>=C){
+                MITERLIMIT=MITERLIMIT-C;
+                time4C++;
+                System.out.println(MITERLIMIT);
+                System.out.println("###############");
+            }
+
+            if (MITERLIMIT<A&&MITERLIMIT<B&&MITERLIMIT<C)
+                break;
+
+            System.out.println("iam not terminatiing");
+        }
+
     }
     static void inputs(){
 
@@ -88,8 +109,5 @@ public class PowerManager {
             if (C>=1&&C<1200)
                 break;
             System.out.println("The rating should be between 1 and 1200... kindly recheck your inputs");
-        }
-
-    }
-        
+        }}
 }
